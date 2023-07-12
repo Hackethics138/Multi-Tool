d68d1605b64578b0015c12bf1597fe3565aaa67f
hackethics138 <139068087+Hackethics138@users.noreply.github.com>
2023-07-13 00:43:42 +0530

    main.py

--- main.py
+++ main.py
@@ -73,7 +73,23 @@ any misuse of this toolkit !
     except:
         print(f"{red}SomeThing Went Wrong...")
 def iptrack():
-    
+    track_ip_address(ip_address):
+    url = f"http://ip-api.com/json/{ip_address}"
+    response = requests.get(url)
+    data = response.json()
+    if data['status'] == 'success':
+        print("IP: ", data['query'])
+        print("Country: ", data['country'])
+        print("Region: ", data['regionName'])
+        print("City: ", data['city'])
+        print("ISP: ", data['isp'])
+    else:
+        print("Failed to track IP address.")
+
+
+ip_address = input("Enter your ip:") 
+track_ip_address(ip_address)
+
 def logic():
     os.system("clear")
     print(meanu)
