9e8f7f920429fed8b15787da887755229e22cdda
hackethics138 <139068087+Hackethics138@users.noreply.github.com>
2023-07-13 00:46:52 +0530

    main.py

--- main.py
+++ main.py
@@ -72,23 +72,6 @@ any misuse of this toolkit !
             about()
     except:
         print(f"{red}SomeThing Went Wrong...")
-def iptrack():
-    track_ip_address(ip_address)
-    url = f"http://ip-api.com/json/{ip_address}"
-    response = requests.get(url)
-    data = response.json()
-    if data['status'] == 'success':
-        print("IP: ", data['query'])
-        print("Country: ", data['country'])
-        print("Region: ", data['regionName'])
-        print("City: ", data['city'])
-        print("ISP: ", data['isp'])
-    else:
-        print("Failed to track IP address.")
-
-
-ip_address = input("Enter your ip:") 
-track_ip_address(ip_address)
 
 def logic():
     os.system("clear")
@@ -167,6 +150,23 @@ def boomber():
         
     logic()
 
+def iptrack():
+    track_ip_address(ip_address)
+    url = f"http://ip-api.com/json/{ip_address}"
+    response = requests.get(url)
+    data = response.json()
+    if data['status'] == 'success':
+        print("IP: ", data['query'])
+        print("Country: ", data['country'])
+        print("Region: ", data['regionName'])
+        print("City: ", data['city'])
+        print("ISP: ", data['isp'])
+    else:
+        print("Failed to track IP address.")
+
+
+ip_address = input("Enter your ip:") 
+track_ip_address(ip_address)
 
 
 if __name__=="__main__":
